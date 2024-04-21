29e2b3914ae915297a5c995ec0edded472b38c96
Crockeo <crockeo@users.noreply.github.com>
2024-04-21 11:18:20 -0700

    implement better file locking (#3)
    Before we were using `fd-lock` which, probably because of it being designed for cross-platform use, does not allow you to atomically upgrade/downgrade file locks between read-only and read-write. The syscall `fcntl` _does_ let you atomically upgrade/downgrade these locks:
    
    > A single process can hold only one type of lock on a file region; if a new lock is applied to an already-locked region, then the existing lock is converted to the new lock type.
    
    So I've re-implemented a file locking interface using `fcntl` directly to let us have atomicity here. This program is designed to work only on posix systems anyways, so :shrug:!

---
+++ src/file_lock.rs
@@ -0,0 +1,120 @@
+//! Provides a file locking interface for files on POSIX-compliant operating systems.
+//! Very similar to the open source fd-lock library,
+//! except that it supports atomically upgrading / downgrading the lock.
+use libc::fcntl;
+use libc::flock;
+use libc::F_SETLKW;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::os::unix::io::AsRawFd;
+use std::path::Path;
+
+pub struct FileLock {
+    file: File,
+}
+
+impl FileLock {
+    pub fn new(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path.as_ref())?;
+        Ok(Self { file })
+    }
+
+    pub fn read(&mut self) -> anyhow::Result<ReadLock> {
+        ReadLock::new(&mut self.file)
+    }
+
+    pub fn write(&mut self) -> anyhow::Result<WriteLock> {
+        WriteLock::new(&mut self.file)
+    }
+}
+
+pub struct ReadLock<'a> {
+    file: Option<&'a mut File>,
+}
+
+impl<'a> ReadLock<'a> {
+    fn new(file: &'a mut File) -> anyhow::Result<Self> {
+        log::debug!("Taking read lock on file {:?}", file);
+        apply_lock(file, LockOperation::Read)?;
+        Ok(Self { file: Some(file) })
+    }
+
+    pub fn upgrade(mut self) -> anyhow::Result<WriteLock<'a>> {
+        log::debug!("Upgrading read lock to write lock on file {:?}", self.file);
+        let file = self.file.take().unwrap();
+        WriteLock::new(file)
+    }
+}
+
+impl Drop for ReadLock<'_> {
+    fn drop(&mut self) {
+        if let Some(ref mut file) = self.file {
+            apply_lock(file, LockOperation::Unlock)
+                .expect("Failed to unlock file during ReadLock Drop");
+        }
+    }
+}
+
+pub struct WriteLock<'a> {
+    file: Option<&'a mut File>,
+}
+
+impl<'a> WriteLock<'a> {
+    fn new(file: &'a mut File) -> anyhow::Result<Self> {
+        log::debug!("Taking write lock on file {:?}", file);
+        apply_lock(file, LockOperation::Write)?;
+        Ok(Self { file: Some(file) })
+    }
+
+    pub fn downgrade(mut self) -> anyhow::Result<ReadLock<'a>> {
+        log::debug!("Downgrading write lock to read lock on file {:?}", self.file);
+        let file = self.file.take().unwrap();
+        ReadLock::new(file)
+    }
+}
+
+impl Drop for WriteLock<'_> {
+    fn drop(&mut self) {
+        if let Some(ref mut file) = self.file {
+            apply_lock(file, LockOperation::Unlock)
+                .expect("Failed to unlock file during WriteLock Drop");
+        }
+    }
+}
+
+enum LockOperation {
+    Read,
+    Write,
+    Unlock,
+}
+
+fn apply_lock(file: &mut File, operation: LockOperation) -> anyhow::Result<()> {
+    let fd = file.as_raw_fd();
+    let lock_type = match operation {
+        LockOperation::Read => libc::F_RDLCK,
+        LockOperation::Write => libc::F_WRLCK,
+        LockOperation::Unlock => libc::F_UNLCK,
+    };
+    let result = unsafe {
+        fcntl(
+            fd,
+            F_SETLKW,
+            &flock {
+                l_type: lock_type,
+                l_whence: libc::SEEK_SET as i16,
+                l_start: 0,
+                l_len: 0,
+                l_pid: 0,
+            },
+        )
+    };
+    if result == -1 {
+        let err = std::io::Error::last_os_error();
+        return Err(err.into())
+    }
+    Ok(())
+}
--- src/venv.rs
+++ src/venv.rs
@@ -1,20 +1,15 @@
+use crate::file_lock::FileLock;
+use crate::file_lock::ReadLock;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 use std::process::ExitStatus;
-use thiserror::Error;
-
-#[derive(Debug, Error, Eq, PartialEq)]
-pub enum Error {
-    #[error("The requested venv didn't exist when we attempted to execute something using it.")]
-    MissingVenv,
-}
 
 pub struct VenvManager {
     path: PathBuf,
-    lock: fd_lock::RwLock<File>,
+    lock: FileLock,
 }
 
 impl VenvManager {
@@ -22,59 +17,22 @@ impl VenvManager {
         let lock_path = path.with_extension("lock");
         Ok(Self {
             path,
-            lock: fd_lock::RwLock::new(File::create(lock_path)?),
+            lock: FileLock::new(lock_path)?,
         })
     }
 
-    pub fn create(&mut self, python_executable: &Path, requirements: &str) -> anyhow::Result<()> {
-        log::debug!("Creating virtual environment at {:?}", self.path);
-        let _write_lock = self.lock.write()?;
-
-        if self.path.join("bin").join("python").exists() {
-            log::debug!("Virtual environment already exists at {:?}", self.path);
-            return Ok(());
-        }
-
-        std::fs::create_dir_all(&self.path)?;
-        let status = Command::new(python_executable)
-            .args(["-m", "venv"])
-            .arg(&self.path)
-            .status()?;
-        anyhow::ensure!(status.success(), "Failed to create virtual environment");
-
-        let requirements_file_path = self.path.with_extension("requirements");
-        {
-            let mut requirements_file = File::create(&requirements_file_path)?;
-            requirements_file.write_all(requirements.as_bytes())?;
-        }
-
-        let venv_pip = self.path.join("bin").join("pip");
-        let status = Command::new(&venv_pip)
-            .args(&["install", "-r"])
-            .arg(requirements_file_path)
-            .status()?;
-        anyhow::ensure!(
-            status.success(),
-            "Failed to pip install requirements into virtual environment"
-        );
-
-        Ok(())
-    }
-
-    pub fn delete(&mut self) -> anyhow::Result<()> {
-        log::debug!("Deleting virtual environment at {:?}", self.path);
-        let _write_lock = self.lock.write()?;
-        std::fs::remove_dir_all(&self.path)?;
-        Ok(())
-    }
-
-    pub fn run(&self, args: &[String]) -> anyhow::Result<ExitStatus> {
+    pub fn run(
+        &mut self,
+        python_executable: &Path,
+        requirements: &str,
+        args: &[String],
+    ) -> anyhow::Result<ExitStatus> {
         log::debug!("Running Python in virtual environment at {:?}", self.path);
-        let _read_lock = self.lock.read()?;
+        let mut _read_lock = self.lock.read()?;
 
         let venv_python = self.path.join("bin").join("python");
         if !venv_python.exists() {
-            return Err(Error::MissingVenv.into());
+            _read_lock = create_venv(_read_lock, python_executable, requirements, &self.path)?;
         }
 
         let status = match Command::new(venv_python).args(args).status() {
@@ -85,6 +43,19 @@ impl VenvManager {
         };
         Ok(status)
     }
+
+    pub fn delete(&mut self) -> anyhow::Result<()> {
+        log::debug!("Deleting virtual environment at {:?}", self.path);
+        let _write_lock = self.lock.write()?;
+        let result = std::fs::remove_dir_all(&self.path);
+        if let Err(err) = result {
+            if err.kind() == std::io::ErrorKind::NotFound {
+                return Ok(());
+            }
+            return Err(err.into())
+        }
+        Ok(())
+    }
 }
 
 pub fn venv_sha(python_executable: &Path, requirements: &str) -> anyhow::Result<String> {
@@ -92,6 +63,47 @@ pub fn venv_sha(python_executable: &Path, requirements: &str) -> anyhow::Result<
     Ok(sha256::digest(format!(
         "{python_version}\n\n{requirements}"
     )))
+}
+
+fn create_venv<'a>(
+    read_lock: ReadLock<'a>,
+    python_executable: &Path,
+    requirements: &str,
+    venv_path: &Path,
+) -> anyhow::Result<ReadLock<'a>> {
+    let write_lock = read_lock.upgrade()?;
+
+    if venv_path.join("bin").join("python").exists() {
+        panic!(
+            "Virtual environment already exists at {:?}. This function shouldn't have been called.",
+            venv_path
+        );
+    }
+
+    std::fs::create_dir_all(venv_path)?;
+    let status = Command::new(python_executable)
+        .args(["-m", "venv"])
+        .arg(venv_path)
+        .status()?;
+    anyhow::ensure!(status.success(), "Failed to create virtual environment");
+
+    let requirements_file_path = venv_path.with_extension("requirements");
+    {
+        let mut requirements_file = File::create(&requirements_file_path)?;
+        requirements_file.write_all(requirements.as_bytes())?;
+    }
+
+    let venv_pip = venv_path.join("bin").join("pip");
+    let status = Command::new(venv_pip)
+        .args(["install", "-r"])
+        .arg(requirements_file_path)
+        .status()?;
+    anyhow::ensure!(
+        status.success(),
+        "Failed to pip install requirements into virtual environment"
+    );
+
+    write_lock.downgrade()
 }
 
 fn python_version(python_executable: &Path) -> anyhow::Result<String> {
